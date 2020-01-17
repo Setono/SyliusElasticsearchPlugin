3cf0ecdc59012788b5743f3d4ba7b0c7879ae057
Peter Bech <peterbechp@gmail.com>
2020-01-17 13:03:43 +0100

    added misiing file

---
+++ src/EventListener/ProductTaxonListener.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusElasticsearchPlugin\EventListener;
+
+use FOS\ElasticaBundle\Persister\ObjectPersisterInterface;
+use Sylius\Component\Core\Model\ProductTaxonInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+
+class ProductTaxonListener
+{
+    /** @var ObjectPersisterInterface */
+    private $persister;
+
+    /** @var bool */
+    private $enabled;
+
+    public function __construct(ObjectPersisterInterface $persister, bool $enabled)
+    {
+        $this->persister = $persister;
+        $this->enabled = $enabled;
+    }
+
+    public function postUpdate(ProductTaxonInterface $productTaxon)
+    {
+        if (!$this->enabled) {
+            return;
+        }
+
+        $product = $productTaxon->getProduct();
+        $this->persister->replaceOne($product);
+    }
+}
