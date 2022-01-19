35e2164ef0a896420318576fd8d745aa0d19fe8c
Igor Mukhin <igor.mukhin@gmail.com>
2022-01-19 10:16:44 +0200

    Updated: Configuration to be compatible with latest symfony/config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,8 +11,13 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('setono_sylius_elasticsearch');
+        if (method_exists(TreeBuilder::class, 'getRootNode')) {
+            $treeBuilder = new TreeBuilder('setono_sylius_elasticsearch');
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $treeBuilder = new TreeBuilder();
+            $rootNode = $treeBuilder->root('setono_sylius_elasticsearch');
+        }
         $rootNode
             ->addDefaultsIfNotSet()
             ->children()
